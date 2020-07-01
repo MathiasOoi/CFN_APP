b1000bf5dd4b56a171c3bcadc3b4a0beeb5cbe05
mathiasooi <mathias7142@gmail.com>
2020-06-30 18:25:56 -0700

    Initial Commit

---
+++ CFN_APP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CFN_APP
+//
+//  Created by Mathias Ooi on 6/30/20.
+//  Copyright © 2020 Mathias Ooi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
